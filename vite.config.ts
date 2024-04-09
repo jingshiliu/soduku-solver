c01699a6fdd580c838f38ca567ad8fe61a53f619
Jingshi Liu <jingshiliu07@gmail.com>
2024-04-09 16:39:24 -0400

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+
+    // un comment following line to make the server accessible from other devices in the same network
+    server: {
+        host: '0.0.0.0',
     },
-  },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 })
